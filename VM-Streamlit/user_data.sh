487455ad4a8e64d7a1a1368f04a5427cb302fe31
YoannAbriel2 <91533461+YoannAbriel2@users.noreply.github.com>
2025-02-11 15:07:09 +0100

    Update user_data.sh

--- VM-Streamlit/user_data.sh
+++ VM-Streamlit/user_data.sh
@@ -3,9 +3,11 @@
 apt-get update && apt upgrade -y
 apt-get install -y python3-pip git
 
-pip install streamlit
+
 git clone https://github.com/julessa/Groupe1_DataBI_Ftech.git
 cd Groupe1_DataBI_Ftech
+pip install streamlit pandas numpy matplotlib seaborn plotly
+
 #pip install -r requirements.txt
 
 nohup streamlit run Streamlit.py --server.enableCORS false --server.address=0.0.0.0 > streamlit.log 2>&1 &
